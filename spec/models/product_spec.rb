c746129011b36b7e5654286d02e7db05476a0b26
Natalia Martian <natalia.martian@gmail.com>
2021-01-12 00:34:11 +0000

    Fix Products validation testing

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,33 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-
   describe 'Validations' do
 
-    xit 'validates :name, :price, :quantity, :category presence: true' do
-      @category = "electronics"
+    it 'validates :name, :price, :quantity, :category presence: true' do
+      @category = Category.create(name: "electronics")
       @product = Product.create(name: "USB typewriter", price: 2350, quantity: 2, category: @category)
-      expect(@product.name, @product.price, @product.quantity, @product.category).not_to eq(nil)
+
+      expect(@product.name).not_to eq(nil)
+      expect(@product.price).not_to eq(nil)
+      expect(@product.quantity).not_to eq(nil)
+      expect(@product.category).not_to eq(nil)
 
     end
     
     it 'validates :name, presence: true' do
+      @category = Category.create(name: "electronics")
       @product = Product.create(price: 2350, quantity: 2, category: @category)
+
       expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     it 'validates :price, presence: true' do
+      @category = Category.create(name: "electronics")
       @product = Product.create(name: "USB typewriter", quantity: 2, category: @category)
+
       expect(@product.errors.full_messages).to include("Price can't be blank")
     end
 
     it 'validates :quantity, presence: true' do
+      @category = Category.create(name: "electronics")
       @product = Product.create(name: "USB typewriter", price: 2350, category: @category)
+
       expect(@product.errors.full_messages).to include("Quantity can't be blank")
     end
 
     it 'validates :category, presence: true' do
+      @category = Category.create(name: "electronics")
       @product = Product.create(name: "USB typewriter", price: 2350, quantity: 2)
+      
       expect(@product.errors.full_messages).to include("Category can't be blank")
     end
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'New user' do
+
+    it 'must have :password field' do
+
+    end
+
+    it 'must have :password_confirmation field' do
+
+    end
+
+    it 'must have matching :password and :password_confirmation fields' do
+
+    end
+
+    xit 'returns error if :password and :password_confirmation fields do not match' do
+
+    end
+
+    xit 'must have unique email' do
+
+    end
+
+    xit 'must return error if email exists in the DB' do
+      
+    end
+
+
+  end
+end
