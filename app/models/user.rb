8e344b003200cb8d0ab430032ce18dd345ab13d4
Natalia Martian <natalia.martian@gmail.com>
2021-01-12 21:30:20 +0000

    Revert "Add more validations for user (one test failing)"
    This reverts commit c8ac1bbb4a32468b19a1e9e2a9a3aebc7009820e.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,5 @@
 class User < ActiveRecord::Base
 
   has_secure_password
-
-  validates_presence_of :first_name, :last_name, :email, :password, :password_confirmation
-  validates_uniqueness_of :email, :message => "Email is in use"
-  validates_length_of :password, minimum: 8
   
 end
